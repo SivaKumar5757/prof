18b6a99e2d19a8a937d575cc70a879af8c2651c7
siva kumar <146540716+SivaKumar5757@users.noreply.github.com>
2024-08-11 23:59:28 +0530

    hi

--- myprof/myprof/settings.py
+++ myprof/myprof/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://sivakumarportfolio.onrender.com',
+]
 
 ROOT_URLCONF = 'myprof.urls'
 
