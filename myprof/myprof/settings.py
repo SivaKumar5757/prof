333b853c74fd9490c2f2128fc8eb487ec253b48c
siva kumar <146540716+SivaKumar5757@users.noreply.github.com>
2024-08-21 10:02:53 +0530

    hi

--- myprof/myprof/settings.py
+++ myprof/myprof/settings.py
@@ -46,7 +46,7 @@ DEBUG = True
 # SECURE_HSTS_PRELOAD = True
 
 
-ALLOWED_HOSTS = ["sivakumar-m.onrender.com","127.0.0.1"]
+ALLOWED_HOSTS = ["sivakumar-m.onrender.com"]
 # Application definition
 
 INSTALLED_APPS = [
